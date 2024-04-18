918f6af8fb5be759500f1d0127d8febaf276fc56
Tan <tan10tvo@gmail.com>
2024-04-18 14:53:50 -0600

    final

---
+++ index.js
@@ -0,0 +1,37 @@
+const http = require('http');
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    const {url}=req;
+
+    switch(url){
+        case '/':
+    
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<h1 style="color: red">Hello World!</h1>');
+    res.write('<p>I wonder what else we can send...</p>');
+    res.write('<di>what up</di>')
+    res.end();
+    break;
+    case '/about':
+
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<h1 style="color: red">Hello World!</h1>');
+    res.write('<p>I wonder what else we can send...</p>');
+    res.write('<di>what up</di>')
+    res.end();
+    break;
+    default:
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('404 - Not Found');
+    }
+  });
+
+  server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+  });
+
